799934514f9d01d6b1f1e86019a288f2e352ccf4
Marat D <Marat@mail.ru>
2026-02-05 23:24:25 +0400

    add Test

---
+++ src/test/java/tests/LoginWithWrongCredentialsTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import core.base.BaseTest;
+import core.pages.LoginPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LoginWithWrongCredentialsTest extends BaseTest {
+    private static LoginPage loginPage;
+
+
+    @BeforeEach
+    public void prepare() {
+        open(baseUrl);
+        loginPage = new LoginPage();
+    }
+
+    @Test
+    public void loginTest(){
+
+        loginPage.login("incorrectUser", "incorrectPassword");
+
+        assertTrue(loginPage.isErrorMessageVisible(), "Сообщение об ошибке входа не отображается");
+
+        String expectedErrorMessage = "Неправильно указан логин и/или пароль";
+        String actualErrorMessage = loginPage.getErrorMessageText();
+        assertEquals(expectedErrorMessage, actualErrorMessage, "Текст об ошибке не совпадает");
+    }
+}
