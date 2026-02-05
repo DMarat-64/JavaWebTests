d2d3ef94f8d51ae3e29f6fb64205b7dcf4d3229c
Marat D <Marat@mail.ru>
2026-02-05 23:24:38 +0400

    add LoginPage

---
+++ src/main/java/core/pages/LoginPage.java
@@ -0,0 +1,94 @@
+package core.pages;
+
+import com.codeborne.selenide.SelenideElement;
+import core.base.BasePage;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class LoginPage extends BasePage {
+
+    private SelenideElement usernameField = $("#field_email");
+    private SelenideElement passwordField = $("#field_password");
+    private SelenideElement loginButton = $("[label='Войти']");
+    private SelenideElement loginButtonQR = $("[label='Войти по QR-коду']");
+    private SelenideElement buttonCantLogin = $("[aria-label='Не получается войти?']");
+    private SelenideElement registrationButton = $$("[class='vkuiButton__in']").get(2);
+
+    private SelenideElement vkButton = $("[data-l='t,vkc']");
+    private SelenideElement mailButton = $("[data-l='t,mailru']");
+    private SelenideElement yandexButton = $("[data-l='t,yandex']");
+
+    private SelenideElement errorMessage = $("[class='LoginForm-module__error___1xmAD" +
+            " vkuiCaption__sizeYNone vkuiCaption__level1 vkuiTypography__host vkuiTypography__normalize vkuiRootComponent__host']");
+
+    {
+        verifyPageElements();
+    }
+
+    @Step("Проверяем видимость всех элементов страницы")
+    public void verifyPageElements() {
+        usernameField.shouldBe(visible);
+        passwordField.shouldBe(visible);
+        loginButton.shouldBe(visible);
+        loginButtonQR.shouldBe(visible);
+        buttonCantLogin.shouldBe(visible);
+        registrationButton.shouldBe(visible);
+        vkButton.shouldBe(visible);
+        mailButton.shouldBe(visible);
+        yandexButton.shouldBe(visible);
+    }
+
+    @Step("Проверяем видимость сообщения об ошибки входа")
+    public boolean isErrorMessageVisible() {
+        return errorMessage.shouldBe(visible).exists();
+    }
+
+    @Step("Получаем текст сообщения об ошибке")
+    public String getErrorMessageText() {
+        return errorMessage.shouldBe(visible).getText();
+    }
+
+    @Step("Входим на сайт с логином и паролем: {username} и {password}")
+    public void login(String username, String password) {
+        usernameField.shouldBe(visible).click();
+        usernameField.shouldBe(visible).setValue(username);
+        passwordField.shouldBe(visible).click();
+        passwordField.shouldBe(visible).setValue(password);
+        loginButton.shouldBe(visible).click();
+    }
+
+    @Step("Входим на сайт без логина")
+    public void loginEmpty () {
+        loginButton.shouldBe(visible).click();
+    }
+
+    @Step("Входим на сайт с логином и без пароля: {username}")
+    public void passwordEmpty(String username) {
+        usernameField.shouldBe(visible).click();
+        usernameField.shouldBe(visible).setValue(username);;
+        loginButton.shouldBe(visible).click();
+    }
+
+    @Step("Переходим на страницу регистрации")
+    public void openRegistrationPage(){
+        registrationButton.shouldBe(visible).click();
+    }
+
+    @Step("Входим на сайт через ВКонтакте")
+    public void loginWithVK() {
+        vkButton.shouldBe(visible).click();
+    }
+
+    @Step("Входим на сайт через Mail")
+    public void loginWithMail() {
+        mailButton.shouldBe(visible).click();
+    }
+
+    @Step("Входим на сайт через Yandex")
+    public void loginWithYandex() {
+        yandexButton.shouldBe(visible).click();
+    }
+}
